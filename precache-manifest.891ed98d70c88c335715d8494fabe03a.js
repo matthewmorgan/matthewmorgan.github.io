c8598e94a54661884b659085d2b3f51e2d0a5c0a
Matthew Morgan <matt@dulcetsoftware.com>
2021-08-27 07:11:22 -0400

    Updates

---
+++ precache-manifest.891ed98d70c88c335715d8494fabe03a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "5edd6189903f6d35a5e0",
+    "url": "/static/css/main.82db92a5.chunk.css"
+  },
+  {
+    "revision": "5edd6189903f6d35a5e0",
+    "url": "/static/js/main.5edd6189.chunk.js"
+  },
+  {
+    "revision": "54618fca19d7adc09d16",
+    "url": "/static/js/1.54618fca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54f5cc54ef936dd0ab64b319fa0167c3",
+    "url": "/static/media/understanding.54f5cc54.webp"
+  },
+  {
+    "revision": "9064f6f57a5752a2fc477867928eec97",
+    "url": "/static/media/semjs.9064f6f5.webp"
+  },
+  {
+    "revision": "3e49b679758c5c7bf2e0ef07b3409a75",
+    "url": "/static/media/exercism.3e49b679.webp"
+  },
+  {
+    "revision": "7dd95a142b92ab72680e88a803cfaa03",
+    "url": "/static/media/breadpos.7dd95a14.webp"
+  },
+  {
+    "revision": "281bcedfa1fc484239bd7b5a02f24abc",
+    "url": "/static/media/buzzdesktop.281bcedf.webp"
+  },
+  {
+    "revision": "64a964ae9e8b86897453df13b79ea4ba",
+    "url": "/static/media/buzzonline.64a964ae.webp"
+  },
+  {
+    "revision": "7bb08dfe6ab842e0ea967cab1233400f",
+    "url": "/static/media/elephantears.7bb08dfe.webp"
+  },
+  {
+    "revision": "ff52616b38c6f447530dd6ac886c829d",
+    "url": "/static/media/honeycreekschool.ff52616b.webp"
+  },
+  {
+    "revision": "03bfc3bee8514210d56a78aa10cc06ee",
+    "url": "/static/media/khanacademy.03bfc3be.webp"
+  },
+  {
+    "revision": "1aca97012ea0693141a0c22da1a8d0dd",
+    "url": "/static/media/morganandyork.1aca9701.webp"
+  },
+  {
+    "revision": "40e8e61ed7e0a84b77c09d770cc57699",
+    "url": "/static/media/roasters.40e8e61e.webp"
+  },
+  {
+    "revision": "f6fedbc9d5705e4aa7614af3989cea1c",
+    "url": "/index.html"
+  }
+];
--- src/dadJokeService.js
+++
@@ -1,9 +0,0 @@
-import superagent from "superagent"
-
-
-export async function getDadJoke() {
-  const resp = await superagent
-      .get('https://icanhazdadjoke.com/')
-      .set('accept', 'text/plain')
-  return resp.text
-}
